3368d7cb45f77a72f17689c23cd03e154740a5df
Imrul Khan <imrulkk69@gmail.com>
2021-09-19 22:11:48 +0600

    api rate limiter implemented

--- app.esm.js
+++ app.esm.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import rateLimit from 'express-rate-limit';
+import dotEnv from 'dotenv';
 
 import fileRouter from './routes/file.esm.js';
 
+dotEnv.config();
+
 const app = express();
+const limiter = rateLimit({
+    windowMs: process.env.API_RATE_LIMIT_MS,
+    max: process.env.API_RATE_LIMIT_HIT
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(limiter);
 
 app.use('/', fileRouter);
 
