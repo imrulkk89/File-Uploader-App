c886f223e74b6fb26df749c63b820a406b33a6a9
Imrul Khan <imrulkk69@gmail.com>
2022-08-03 11:09:03 +0600

    cron job enabled

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { cronJob } = require('./cron.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//cronJob();
+cronJob();
 
 const HOST = (process.env.HOST) || 'localhost';
 const PORT = (process.env.PORT) || 3000;
